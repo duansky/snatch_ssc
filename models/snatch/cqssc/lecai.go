4e0753e789a7a8124f06e91fc13ccdcd6cac386f
duansky <duansky@gmail.com>
2016-09-14 16:21:39 +0800

    完成lecai抓取

--- models/snatch/cqssc/lecai.go
+++ models/snatch/cqssc/lecai.go
@@ -2,7 +2,6 @@ package cqssc
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
 	"reflect"
 	"snatch_ssc/ioc"
@@ -17,15 +16,15 @@ import (
 )
 
 // 乐彩官网
-type LeicaiSnatch struct {
+type LecaiSnatch struct {
 }
 
 func init() {
-	ioc.Register("snatch.ssc.cq.leicai", reflect.TypeOf(new(LeicaiSnatch)))
+	ioc.Register("snatch.ssc.cq.leicai", reflect.TypeOf(new(LecaiSnatch)))
 }
 
 // 抓取网页
-func (this *LeicaiSnatch) Snatch() (string, error) {
+func (this *LecaiSnatch) Snatch() (string, error) {
 	header := make(http.Header)
 	header.Set("Referer", "http://baidu.lecai.com/lottery/draw/view/200")
 	doc, err := goquery.NewDocumentWithHeader("http://baidu.lecai.com/lottery/draw/sorts/ajax_get_draw_data.php?lottery_type=200&date="+time.Now().Format("2006-01-02"), header)
@@ -33,40 +32,43 @@ func (this *LeicaiSnatch) Snatch() (string, error) {
 		beego.Error(err)
 		return "", err
 	}
-	fmt.Println("===lc:", doc.Text())
+
 	return doc.Text(), nil
 }
 
 // 解析网页数据
-func (this *LeicaiSnatch) Resolve(content string) (datas []*inter.SscData) {
+func (this *LecaiSnatch) Resolve(content string) (datas []*inter.SscData) {
 	datas = make([]*inter.SscData, 0, 10)
 	if !sys.HasValue(content) {
 		return datas
 	}
 
-	doc, err := goquery.NewDocumentFromReader(strings.NewReader(content))
-	if err != nil {
-		beego.Error(err)
+	var l struct {
+		Data struct {
+			Data []struct {
+				Phase  string `json:"phase"`
+				Result struct {
+					Result []struct {
+						Data []string `json:"data"`
+					} `json:"result"`
+				} `json:"result"`
+			} `json:"data"`
+		} `json:"data"`
 	}
+	json.Unmarshal([]byte(content), &l)
 
-	doc.Find("#openlist").Children().Each(func(i int, s *goquery.Selection) {
+	for _, d := range l.Data.Data {
 		data := new(inter.SscData)
-		// For each item found
-		if i > 0 {
-			no, _ := s.Children().Eq(0).Html()
-			data.No.SetValue(no)
+		data.No.SetValue(d.Phase[2:])
+		data.Results.SetValue(strings.Join(d.Result.Result[0].Data, ","))
 
-			results, _ := s.Children().Eq(1).Html()
-			data.Results.SetValue(strings.Replace(results, "-", ",", -1))
-
-			datas = append(datas, data)
-		}
-	})
+		datas = append(datas, data)
+	}
 
 	return datas
 }
 
-func (this *LeicaiSnatch) Processing(datas []*inter.SscData) {
+func (this *LecaiSnatch) Processing(datas []*inter.SscData) {
 	j, _ := json.Marshal(datas)
 	beego.Info(string(j))
 }
--- models/snatch/init.go
+++ models/snatch/init.go
@@ -15,7 +15,7 @@ func Proccess() error {
 
 	/********* 读取配置分别启动CQ、BJ、GX等采集 **********/
 	items := beego.AppConfig.Strings("snatch::data.collection.item")
-	fmt.Println(items)
+	beego.Info("采集项:", items)
 
 	for _, item := range items {
 		if obj, ok := ioc.Create(fmt.Sprintf("snatch.ssc.%s", item)); ok {
